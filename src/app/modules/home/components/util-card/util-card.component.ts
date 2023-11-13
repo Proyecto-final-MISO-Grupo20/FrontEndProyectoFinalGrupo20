ce1980857ceca4cb19d4df7bb67ce545b8c50c65
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2023-11-12 21:14:52 -0500

    [Feature] Add filter cards

--- src/app/modules/home/components/util-card/util-card.component.ts
+++ src/app/modules/home/components/util-card/util-card.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from 'ui';
+import { CardType } from '../../utils/card-type.enum';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-util-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiModule, FormsModule],
   templateUrl: './util-card.component.html',
   styleUrls: ['./util-card.component.scss'],
 })
-export class UtilCardComponent {}
+export class UtilCardComponent {
+  @Input() headerTitle = 'Title';
+  @Input() type = CardType.search;
+
+  // checkbox
+  @Input() options: any;
+  selectedOptions: any[] = [];
+
+  // search
+  inputSearch!: string;
+
+  get cardType() {
+    return CardType;
+  }
+}
--- src/app/modules/home/pages/business-home/business-home.component.ts
+++ src/app/modules/home/pages/business-home/business-home.component.ts
@@ -2,20 +2,33 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusinessDataViewComponent } from '../../components/business-data-view/business-data-view/business-data-view.component';
 import { BusinessHomeService } from '../../services/business-home/business-home.service';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { UtilCardComponent } from '../../components/util-card/util-card.component';
+import { CardType } from '../../utils/card-type.enum';
+import { TechnicalLanguagesService } from '../../../technical-data/services/technical-languages/technical-languages.service';
+import { Skill } from '../../../technical-data/models/skills';
 
 @Component({
   selector: 'app-business-home',
   standalone: true,
-  imports: [CommonModule, BusinessDataViewComponent],
+  imports: [CommonModule, BusinessDataViewComponent, UtilCardComponent],
   templateUrl: './business-home.component.html',
   styleUrls: ['./business-home.component.scss'],
 })
 export class BusinessHomeComponent implements OnInit {
   businessHomeService = inject(BusinessHomeService);
+  technicalLanguagesService = inject(TechnicalLanguagesService);
+
+  languages$!: Observable<Skill[]>;
+
   candidatesData!: any;
 
+  get cardType() {
+    return CardType;
+  }
+
   ngOnInit(): void {
+    this.getLanguages();
     this.getCandidatesData();
   }
 
@@ -24,5 +37,9 @@ export class BusinessHomeComponent implements OnInit {
       .getCandidates()
       .pipe(tap((candidates) => (this.candidatesData = candidates)))
       .subscribe();
+  }
+
+  getLanguages() {
+    this.languages$ = this.technicalLanguagesService.getLanguages();
   }
 }
