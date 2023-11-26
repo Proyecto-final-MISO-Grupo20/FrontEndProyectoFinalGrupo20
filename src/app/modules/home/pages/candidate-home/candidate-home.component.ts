d2184352278265335c5cdca0706f24f2ad43c9ff
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2023-11-26 15:35:22 -0500

    [Refactor] Update Candidate Home

--- src/app/modules/home/pages/candidate-home/candidate-home.component.ts
+++ src/app/modules/home/pages/candidate-home/candidate-home.component.ts
@@ -1,28 +1,101 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CandidateDataViewComponent } from '../../components/candidate-data-view/candidate-data-view.component';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { CandidateHomeService } from '../../services/candidate-home/candidate-home.service';
+import { LanguageModule } from 'language';
+import { UtilCardComponent } from '../../components/util-card/util-card.component';
+import { CardType } from '../../utils/card-type.enum';
+import { Skill, SkillType } from '../../../technical-data/models/skills';
+import { TechnicalLanguagesService } from '../../../technical-data/services/technical-languages/technical-languages.service';
+import { MessageService } from 'primeng/api';
+import { UiModule } from 'ui';
 
 @Component({
   selector: 'app-candidate-home',
   standalone: true,
-  imports: [CommonModule, CandidateDataViewComponent],
+  providers: [MessageService],
+  imports: [
+    CommonModule,
+    CandidateDataViewComponent,
+    LanguageModule,
+    UtilCardComponent,
+    UiModule,
+  ],
   templateUrl: './candidate-home.component.html',
   styleUrls: ['./candidate-home.component.scss'],
 })
-export class CandidateHomeComponent {
+export class CandidateHomeComponent implements OnInit {
   candidateHomeService = inject(CandidateHomeService);
+  technicalLanguagesService = inject(TechnicalLanguagesService);
+  messageService = inject(MessageService);
+
   offersData!: any;
+  languages$!: Observable<Skill[]>;
+  loading = false;
+  filteredData!: any;
+
+  get cardType() {
+    return CardType;
+  }
+
+  get skillType() {
+    return SkillType;
+  }
 
   ngOnInit(): void {
+    this.getLanguages();
     this.getOffersData();
   }
 
   getOffersData() {
     this.candidateHomeService
       .getOffers()
-      .pipe(tap((offers) => (this.offersData = offers)))
+      .pipe(
+        tap((offers) => {
+          this.offersData = offers;
+          this.filteredData = offers;
+        })
+      )
       .subscribe();
+  }
+
+  getLanguages() {
+    this.languages$ = this.technicalLanguagesService
+      .getLanguages()
+      .pipe(tap(() => (this.loading = false)));
+  }
+
+  onSearchChange(search: any) {
+    this.filteredData = [...this.offersData];
+
+    if (search.length > 0) {
+      this.filteredData = this.filteredData.filter((offer: any) =>
+        offer.perfil.toLocaleLowerCase().match(search.toLocaleLowerCase())
+      );
+    } else {
+      this.filteredData = [...this.offersData];
+    }
+  }
+
+  apply(offer: any) {
+    this.candidateHomeService
+      .assignCandidateToOffer({ ofertaId: offer.id })
+      .subscribe({
+        next: (res) => {
+          this.show('success', 'Success', 'Aplicaste a la oferta');
+        },
+        error: (err) => {
+          this.show('error', 'Error', 'Ya has aplicado a esta oferta');
+        },
+      });
+  }
+
+  show(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      severity,
+      summary,
+      detail,
+    });
   }
 }
